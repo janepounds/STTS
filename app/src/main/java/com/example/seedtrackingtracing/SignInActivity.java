a5a753265b9afc0b24594045a388d197e06f2e62
Musa Rahim <musarahim13@gmail.com>
2020-05-15 09:38:26 +0300

    redesigned the login interface

--- app/src/main/java/com/example/seedtrackingtracing/SignInActivity.java
+++ app/src/main/java/com/example/seedtrackingtracing/SignInActivity.java
@@ -9,6 +9,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,20 +22,24 @@ public class SignInActivity extends AppCompatActivity {
     private EditText SignInMail, SignInPass;
     private FirebaseAuth auth;
     private TextView SignInButton, signuptxt, resetPassword;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        getSupportActionBar().hide();
         auth = FirebaseAuth.getInstance();
         SignInMail = (EditText) findViewById(R.id.usernameedt);
         SignInPass = (EditText) findViewById(R.id.passwordedt);
         SignInButton = (TextView) findViewById(R.id.signinbtn);
         signuptxt = (TextView) findViewById(R.id.registertv);
         resetPassword = (TextView) findViewById(R.id.resetPasswordTv);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
         SignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
                 String email = SignInMail.getText().toString();
                 final String password = SignInPass.getText().toString();
                 if (TextUtils.isEmpty(email)) {
@@ -52,14 +57,16 @@ public class SignInActivity extends AppCompatActivity {
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (!task.isSuccessful()) {
                                     // there was an error
+                                    progressBar.setVisibility(View.GONE);
                                     if (password.length() < 8) {
                                         Toast.makeText(getApplicationContext(), "Password must be more than 8 digit", Toast.LENGTH_SHORT).show();
                                     } else {
-                                        Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(getApplicationContext(), "Error, please check your username and password", Toast.LENGTH_SHORT).show();
                                     }
                                 } else {
                                     Intent intent = new Intent(SignInActivity.this, MainActivity.class);
                                     startActivity(intent);
+                                    progressBar.setVisibility(View.GONE);
                                     finish();
                                 }
                             }
